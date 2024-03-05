47e199c5168e71e275296f67cd881fdeff323032
Jacob Leonardo <leonardojacob257@gmail.com>
2024-03-05 11:20:58 -0700

    Piercing bullets

--- Assets/Scripts/Bullets/Bullet.cs
+++ Assets/Scripts/Bullets/Bullet.cs
@@ -7,36 +7,56 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] protected float _damageMod = 1;
-    [SerializeField] bool _destroyOnHit = true;
+    [SerializeField] bool wallPiercing = false;
+    [SerializeField] int piercingPower = 0; // How many enemies the bullet can pierce through
+    int currentPiercingPower;
 
     public int FirearmDamage { get; set; }
     public float LifeSpan { get; set; }
 
     public Rigidbody2D _rb;
-
-    void Update()
-    {
-        
-    }
+    public Collider2D collider;
 
     void Start()
     {
         StartCoroutine(lifeStart());
         TryGetComponent(out Rigidbody2D _rb);
+        TryGetComponent(out Collider2D collider);
+        if(piercingPower > 0)
+        {
+            collider.isTrigger = true;
+        }
     }
 
     private IEnumerator lifeStart()
     {
+        currentPiercingPower = piercingPower;
         yield return new WaitForSeconds(LifeSpan);
         Destroy(gameObject);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.TryGetComponent(out Health targetHealth))
-        {
-            targetHealth.TakeDamage(FirearmDamage * _damageMod);
-        }
-        if(_destroyOnHit) Destroy(gameObject);
+        OnHit(collision.collider);
     }
+
+	private void OnTriggerEnter2D(Collider2D collider)
+	{
+        OnHit(collider);
+	}
+
+    private void OnHit(Collider2D collision)
+    {
+		if (collision.gameObject.TryGetComponent(out Health targetHealth))
+		{
+			currentPiercingPower--;
+			targetHealth.TakeDamage(FirearmDamage * _damageMod);
+		}
+		else if (collision.gameObject.layer == LayerMask.NameToLayer("World"))
+		{
+			if (wallPiercing) currentPiercingPower--;
+			else Destroy(gameObject);
+		}
+		if (currentPiercingPower < 0) Destroy(gameObject);
+	}
 }
