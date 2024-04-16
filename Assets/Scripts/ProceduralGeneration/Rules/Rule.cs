df02c021ae6dee04d5b5cb1018270d53500fe588
Jacob Leonardo <leonardojacob257@gmail.com>
2024-04-16 17:45:38 -0600

    LSystem

---
+++ Assets/Scripts/ProceduralGeneration/Rules/Rule.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="ProceduralCity/Rule")]
+public class Rule : ScriptableObject
+{
+    public string letter;
+    [SerializeField] private string[] results = null;
+    [SerializeField] private bool randomResult = false;
+
+    public string GetResult()
+    {
+        if(!randomResult) return results[0];
+
+        int randomIndex = Random.Range(0, results.Length);
+        return results[randomIndex];
+    }
+}
