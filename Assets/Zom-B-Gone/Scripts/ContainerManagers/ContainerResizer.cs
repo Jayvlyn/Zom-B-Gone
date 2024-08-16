997a87858239a3a28b36be6d3df9c284f0a6d1ac
Jacob Leonardo <leonardojacob257@gmail.com>
2024-08-16 00:14:44 -0600

    Better row removal

--- Assets/Zom-B-Gone/Scripts/ContainerManagers/ContainerResizer.cs
+++ Assets/Zom-B-Gone/Scripts/ContainerManagers/ContainerResizer.cs
@@ -26,74 +26,109 @@ public class ContainerResizer : MonoBehaviour
 		}
 		else if (fullSlotsInLastRow == 0)
 		{
-			int fullSlotsInSecondLastRow = 0;
-			for (int i = lastSlotIndex-rowSize; i > (lastSlotIndex - rowSize - rowSize); i--)
+			// tiny tiny bug with this method, a specific case where it will leave two empty rows instead of one, not that serious
+
+			// start at the last slot excluding the very last row
+			int secondLastRowEndSlotIndex = lastSlotIndex - rowSize;
+			int emptySlots = 0;
+
+			for (int i = secondLastRowEndSlotIndex; i > 0; i--)
 			{
-				if (containerData.Container.collectibleSlots[i].collectible != null)
+                if (containerData.Container.collectibleSlots[i].collectible == null)
+                {
+                    emptySlots++;
+                }
+				else
 				{
-					fullSlotsInSecondLastRow++;
+                    // Once you find a non empty slot, stop checking for more empty slots
+                    break;
 				}
-			}
-			if(fullSlotsInSecondLastRow == 0 && (lastSlotIndex + rowSize + 1) >= rowSize)
+            }
+
+			if(emptySlots > rowSize) // at least one empty row to remove
 			{
-				ReduceRow();
+				RemoveRows(emptySlots / rowSize);
 			}
 		}
 	}
 
-	private void ReduceRow()
-	{
-		CollectibleSlot[] cachedSlots = containerData.Container.collectibleSlots;
+    private void IncreaseRow()
+    {
+        CollectibleSlot[] cachedSlots = containerData.Container.collectibleSlots;
 
-		containerData.size -= rowSize;
-		containerData.Container = new CollectibleContainer(containerData.size);
-		containerData.Container.OnCollectibleUpdated += containerData.onContainerCollectibleUpdated.Raise;
-		containerData.Container.OnCollectibleSwapped += containerData.onContainerCollectibleSwapped.Raise;
+        containerData.size += rowSize;
+        containerData.Container = new CollectibleContainer(containerData.size);
+        containerData.Container.OnCollectibleUpdated += containerData.onContainerCollectibleUpdated.Raise;
+        containerData.Container.OnCollectibleSwapped += containerData.onContainerCollectibleSwapped.Raise;
 
-		for (int i = 0; i < cachedSlots.Length - rowSize; i++)
-		{
-			containerData.Container.collectibleSlots[i] = cachedSlots[i];
-		}
+        for (int i = 0; i < cachedSlots.Length; i++)
+        {
+            containerData.Container.collectibleSlots[i] = cachedSlots[i];
+        }
+
+        // Makes an empty slot with the same allows
+        CollectibleSlot slotTemplate = cachedSlots[0];
+        slotTemplate.collectible = null;
+        slotTemplate.quantity = 0;
+
+        for (int i = (cachedSlots.Length + rowSize - 1); i >= cachedSlots.Length; i--)
+        {
+            containerData.Container.collectibleSlots[i] = slotTemplate;
+        }
+
+        // loop and instantiate more slots, making them children of this
+        Object newSlot = Resources.Load(slotPrefabName);
+        int lastSlotIndex = containerData.Container.collectibleSlots.Length - 1 + rowSize;
+        for (int i = lastSlotIndex; i > (lastSlotIndex - rowSize); i--)
+        {
+            // new slot with this as the parent
+            Instantiate(newSlot, transform);
+        }
+    }
+
+    private void RemoveRows(int rowAmountToRemove)
+    {
+        CollectibleSlot[] cachedSlots = containerData.Container.collectibleSlots;
+
+		int slotAmountToRemove = rowSize * rowAmountToRemove;
+
+        containerData.size -= slotAmountToRemove;
+        containerData.Container = new CollectibleContainer(containerData.size);
+        containerData.Container.OnCollectibleUpdated += containerData.onContainerCollectibleUpdated.Raise;
+        containerData.Container.OnCollectibleSwapped += containerData.onContainerCollectibleSwapped.Raise;
+
+        for (int i = 0; i < cachedSlots.Length - slotAmountToRemove; i++)
+        {
+            containerData.Container.collectibleSlots[i] = cachedSlots[i];
+        }
 
 
-		int lastSlotIndex = containerData.Container.collectibleSlots.Length - 1 + rowSize;
-		for (int i = lastSlotIndex; i > (lastSlotIndex - rowSize); i--)
-		{
-			Destroy(transform.GetChild(i).gameObject);
-		}
-	}
+        int lastSlotIndex = containerData.Container.collectibleSlots.Length - 1 + slotAmountToRemove;
+        for (int i = lastSlotIndex; i > (lastSlotIndex - slotAmountToRemove); i--)
+        {
+            Destroy(transform.GetChild(i).gameObject);
+        }
+    }
 
-	private void IncreaseRow()
-	{
-		CollectibleSlot[] cachedSlots = containerData.Container.collectibleSlots;
+    private void RemoveRow()
+    {
+        CollectibleSlot[] cachedSlots = containerData.Container.collectibleSlots;
 
-		containerData.size += rowSize;
-		containerData.Container = new CollectibleContainer(containerData.size);
-		containerData.Container.OnCollectibleUpdated += containerData.onContainerCollectibleUpdated.Raise;
-		containerData.Container.OnCollectibleSwapped += containerData.onContainerCollectibleSwapped.Raise;
+        containerData.size -= rowSize;
+        containerData.Container = new CollectibleContainer(containerData.size);
+        containerData.Container.OnCollectibleUpdated += containerData.onContainerCollectibleUpdated.Raise;
+        containerData.Container.OnCollectibleSwapped += containerData.onContainerCollectibleSwapped.Raise;
 
-		for (int i = 0; i < cachedSlots.Length; i++)
-		{
-			containerData.Container.collectibleSlots[i] = cachedSlots[i];
-		}
+        for (int i = 0; i < cachedSlots.Length - rowSize; i++)
+        {
+            containerData.Container.collectibleSlots[i] = cachedSlots[i];
+        }
 
-		// Makes an empty slot with the same allows
-		CollectibleSlot slotTemplate = cachedSlots[0];
-		slotTemplate.collectible = null;
-		slotTemplate.quantity = 0;
 
-		for (int i = (cachedSlots.Length+rowSize-1); i>=cachedSlots.Length; i--)
-		{
-			containerData.Container.collectibleSlots[i] = slotTemplate;
-		}
-
-		// loop and instantiate more slots, making them children of this
-		Object newSlot = Resources.Load(slotPrefabName);
-		int lastSlotIndex = containerData.Container.collectibleSlots.Length - 1 + rowSize;
-		for (int i = lastSlotIndex; i > (lastSlotIndex - rowSize); i--)
-		{
-			// new slot with this as the parent
-			Instantiate(newSlot, transform);
-		}
-	}
+        int lastSlotIndex = containerData.Container.collectibleSlots.Length - 1 + rowSize;
+        for (int i = lastSlotIndex; i > (lastSlotIndex - rowSize); i--)
+        {
+            Destroy(transform.GetChild(i).gameObject);
+        }
+    }
 }
