ff20d970e9938154708960b616520a8066dc3a03
Jacob Leonardo <leonardojacob257@gmail.com>
2024-10-05 12:53:01 -0600

    Bug fixing

--- Assets/Zom-B-Gone/Scripts/Vehicle/VanFloor.cs
+++ Assets/Zom-B-Gone/Scripts/Vehicle/VanFloor.cs
@@ -4,80 +4,98 @@ using UnityEngine;
 
 public class VanFloor : Floor
 {
-    public Vehicle vehicle;
-    public SpriteRenderer vanRoofSprite;
+	public Vehicle vehicle;
+	public SpriteRenderer vanRoofSprite;
 
+	private Coroutine hideRoofCoroutine;
+	private Coroutine showRoofCoroutine;
 
-    // make sure collectibles with multiple colliders only have 1 that interacts with floor container's collider
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        // if player is driving vehicle, or vehicle is moving, dont do anything, dont want to pick stuff up by moving over it
-        if (vehicle.Active) return;
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (vehicle.Active) return;
 
-        if (collision.CompareTag("Player"))
-        {
-            PlayerController pc = collision.GetComponent<PlayerController>();
-            StopCoroutine("ShowRoof");
-			if (gameObject.activeInHierarchy) StartCoroutine(HideRoof(2));
-        }
-        else if (vehicle.rb.velocity.magnitude > 0.2)
-        {
-            return;
-        }
-        else if (collision.gameObject.layer == LayerMask.NameToLayer("AirborneItem") || collision.gameObject.layer == LayerMask.NameToLayer("InteractableItem"))
-        {
-            GameObject itemObject = collision.gameObject;
-            Item item = itemObject.GetComponent<Item>();
-            if (item.currentState == Item.ItemState.HELD)
-            {
-                item.vanBack = this;
-            }
-            else
-            {
-                StartCoroutine(AddToBack(item));
-                StartCoroutine(AddToFloorContainer(collision));
-            }
-        }
-        else if (collision.gameObject.layer == LayerMask.NameToLayer("Interactable"))
-        {
-            StartCoroutine(AddToFloorContainer(collision));
-        }
+		if (collision.CompareTag("Player"))
+		{
+			PlayerController pc = collision.GetComponent<PlayerController>();
 
-    }
+			if (showRoofCoroutine != null)
+			{
+				StopCoroutine(showRoofCoroutine);
+				showRoofCoroutine = null;
+			}
 
+			if (hideRoofCoroutine == null && gameObject.activeInHierarchy)
+			{
+				hideRoofCoroutine = StartCoroutine(HideRoof(2));
+			}
+		}
+		else if (vehicle.rb.velocity.magnitude > 0.2)
+		{
+			return;
+		}
+		else if (collision.gameObject.layer == LayerMask.NameToLayer("AirborneItem") || collision.gameObject.layer == LayerMask.NameToLayer("InteractableItem"))
+		{
+			GameObject itemObject = collision.gameObject;
+			Item item = itemObject.GetComponent<Item>();
+			if (item.currentState == Item.ItemState.HELD)
+			{
+				item.vanBack = this;
+			}
+			else
+			{
+				StartCoroutine(AddToBack(item));
+				StartCoroutine(AddToFloorContainer(collision));
+			}
+		}
+		else if (collision.gameObject.layer == LayerMask.NameToLayer("Interactable"))
+		{
+			StartCoroutine(AddToFloorContainer(collision));
+		}
+	}
 
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player"))
+		{
+			if (hideRoofCoroutine != null)
+			{
+				StopCoroutine(hideRoofCoroutine);
+				hideRoofCoroutine = null;
+			}
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            StopCoroutine("HideRoof");
-			if (gameObject.activeInHierarchy) StartCoroutine(ShowRoof(2));
-        }
-    }
+			if (showRoofCoroutine == null && gameObject.activeInHierarchy)
+			{
+				showRoofCoroutine = StartCoroutine(ShowRoof(2));
+			}
+		}
+	}
 
-    private IEnumerator HideRoof(float speed)
-    {
-        while(vanRoofSprite.color.a > 0.1)
-        {
-            vanRoofSprite.color = new Color(vanRoofSprite.color.r, vanRoofSprite.color.g, vanRoofSprite.color.b, vanRoofSprite.color.a - Time.deltaTime * speed);
-            yield return null;
-        }
-    }
+	private IEnumerator HideRoof(float speed)
+	{
+		while (vanRoofSprite.color.a > 0.1f)
+		{
+			vanRoofSprite.color = new Color(vanRoofSprite.color.r, vanRoofSprite.color.g, vanRoofSprite.color.b, vanRoofSprite.color.a - Time.deltaTime * speed);
+			yield return null;
+		}
 
-    private IEnumerator ShowRoof(float speed)
-    {
-        while (vanRoofSprite.color.a < 1)
-        {
-            vanRoofSprite.color = new Color(vanRoofSprite.color.r, vanRoofSprite.color.g, vanRoofSprite.color.b, vanRoofSprite.color.a + Time.deltaTime * speed);
-            yield return null;
-        }
-    }
+		hideRoofCoroutine = null;
+	}
 
-    public IEnumerator AddToBack(Item item)
-    {
-        yield return new WaitForSeconds(2);
-        item.transform.parent = transform;
-        item.AddToVan();
-    }
+	private IEnumerator ShowRoof(float speed)
+	{
+		while (vanRoofSprite.color.a < 1f)
+		{
+			vanRoofSprite.color = new Color(vanRoofSprite.color.r, vanRoofSprite.color.g, vanRoofSprite.color.b, vanRoofSprite.color.a + Time.deltaTime * speed);
+			yield return null;
+		}
+
+		showRoofCoroutine = null;
+	}
+
+	public IEnumerator AddToBack(Item item)
+	{
+		yield return new WaitForSeconds(2);
+		item.transform.parent = transform;
+		item.AddToVan();
+	}
 }
