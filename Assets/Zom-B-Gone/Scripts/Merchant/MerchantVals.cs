a171f420dc103efe2bd420893f709f8f56620679
Jacob Leonardo <leonardojacob257@gmail.com>
2024-11-11 20:52:25 -0700

    Update MerchantVals.cs

--- Assets/Zom-B-Gone/Scripts/Merchant/MerchantVals.cs
+++ Assets/Zom-B-Gone/Scripts/Merchant/MerchantVals.cs
@@ -9,9 +9,9 @@ public class MerchantVals
 {
     [HideInInspector] public int reputationLevel;
     [HideInInspector] public int reputationExp;
-    [OdinSerialize] public Dictionary<CollectibleData, int> buyOffers = new Dictionary<CollectibleData, int>(); // collectible and price
-	[OdinSerialize] public Dictionary<CollectibleData, int> inventory = new Dictionary<CollectibleData, int>(); // collectible and amount
-	[OdinSerialize] public Dictionary<CollectibleData, int> prices = new Dictionary<CollectibleData, int>(); // collectible and price
+    public Dictionary<CollectibleData, int> buyOffers = new Dictionary<CollectibleData, int>(); // collectible and price
+	public Dictionary<CollectibleData, int> inventory = new Dictionary<CollectibleData, int>(); // collectible and amount
+	public Dictionary<CollectibleData, int> prices = new Dictionary<CollectibleData, int>(); // collectible and price
 
     public int GetNextLevelRequirement()
     {
